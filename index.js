8f4fe63117aef449756f677844debc47cba0336c
Giwa Abdulsamad <giwaabdulsamad0@gmail.com>
2024-08-14 13:31:05 +0000

    nav bar for smaller screens

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 let fullname = document.querySelector("#label");
 let email = document.querySelector("#email");
 let message = document.querySelector("#text");
-let login = document.querySelector('.section');
-let closeLogin = document.querySelector('.exit');
+let login = document.querySelector(".display-log");
+let login2 = document.querySelector(".section-rel");
+let closeLogin = document.querySelector(".exit");
+let menu_button = document.querySelector("#menu-button");
+let side_nav = document.querySelector(".side-nav");
+let side_nav_exit = document.querySelector(".exit");
 // let inputs = document.getElementsByTagName('input');
 function move() {
   fullname.style.top = "-10px";
@@ -24,10 +28,23 @@ function back3() {
   message.style.top = "160px";
 }
 
-
-function showLog(){
-  login.style.display = 'block';
+function showLog1() {
+  login2.style.display = "block";
 }
-function closeLog(){
-  login.style.display = 'none';
+function showLog() {
+  login.style.display = "block";
+}
+function closeLog1() {
+  login2.style.display = "none";
+}
+function closeLog() {
+  side_nav.style.display = "none";
+  login.style.display = "none";
+}
+
+function showsidenav() {
+  side_nav.style.display = "block";
+}
+function closesidenav() {
+  side_nav.style.display = "none";
 }
